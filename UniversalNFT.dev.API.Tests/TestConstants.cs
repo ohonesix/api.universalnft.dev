44076deeda5de3b934ba9443e40c562bb1fc7bac
Jacob Pretorius <17687841+jacobpretorius@users.noreply.github.com>
2024-01-28 14:22:41 +0000

    support to 12

--- UniversalNFT.dev.API.Tests/TestConstants.cs
+++ UniversalNFT.dev.API.Tests/TestConstants.cs
@@ -2,21 +2,32 @@
 {
     public static class TestConstants
     {
+        // Token stuff
         public const string TokenId = "0008555555555555555555555555555555555555555555555555555555555555";
 
         public const string TokenOwner = "rnohonesixOHONESIX016016016ohonesix";
 
         public const string TokenIssuer = "rnissue016OHONESIX016016016ohonesix";
 
+        // META stuff
         public const string MetaIpfs = "ipfs://ohonesixOHONESIXohonesixOHONESIXohonesix016016";
+
+        public const string MetaIpfsWithFile = "ipfs://ohonesixOHONESIXohonesixOHONESIXohonesix016016/meta.json";
 
         public const string MetaIpfsRaw = "ohonesixOHONESIXohonesixOHONESIXohonesix016016";
 
         public const string MetaIpfsRawWithFile = "ohonesixOHONESIXohonesixOHONESIXohonesix016016/016.png";
 
-        public const string MetaNormalisedIpfsUrl = "https://ipfs.io/ipfs/metasixOHONESIXohonesixOHONESIXohonesix016016";
+        public const string MetaNormalisedIpfsUrl = "https://ipfs.io/ipfs/ohonesixOHONESIXohonesixOHONESIXohonesix016016";
+
+        public const string MetaNormalisedIpfsUrlWithFile = "https://ipfs.io/ipfs/ohonesixOHONESIXohonesixOHONESIXohonesix016016/meta.json";
 
         public const string MetaJsonUrl = "https://ohonesix.com/example/file.json";
+
+        // Image stuff
+        public const string IpfsWithFile = "ipfs://ohonesixOHONESIXohonesixOHONESIXohonesix016016/016.png";
+
+        public const string IpfsDoubleWithFile = "ipfs://ipfs/ohonesixOHONESIXohonesixOHONESIXohonesix016016/016.png";
 
         public const string NormalisedIpfsUrl = "https://ipfs.io/ipfs/ohonesixOHONESIXohonesixOHONESIXohonesix016016";
 
